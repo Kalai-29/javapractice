9246ab57ecf0dbaca5779b6b74234085c96d8dac
Kalai-29 <kalaiyarasikk06@gmail.com>
2024-11-27 13:33:25 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+package firstpack;
+
+public class if condition{ 
+
+	public static void main(String[] args) {
+		int a=10;
+		if(a<15) {
+			system.out.println("hello good morning")
+		}
+
+	}
+
+}
